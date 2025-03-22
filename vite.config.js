eaef4aba83727edb9bc12083be1ac6fb2af2aa04
Ruben Soler <rubensolert@gmail.com>
2025-03-22 10:20:17 -0500

    modifica vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // 👈 Importante para Railway o despliegues fuera de la raíz
 })
