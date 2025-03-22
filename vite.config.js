674b55b90d1aa6c5c849b76e19a0e070e4a05f12
Ruben Soler <rubensolert@gmail.com>
2025-03-22 10:47:01 -0500

    la quinta es la vencida

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // 👈 Importante para Railway o despliegues fuera de la raíz
+  base: './', // 🔹 Muy importante si no usas rutas absolutas
+  build: {
+    outDir: 'dist'
+  }
 })
